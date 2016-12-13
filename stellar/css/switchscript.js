0ba866652b85cab5110819ac95446603fc71aad4
aliciaaarnio <alicia.aarnio@gmail.com>
2016-12-12 22:25:58 -0700

    still messing with cookies

--- stellar/css/switchscript.js
+++ stellar/css/switchscript.js
@@ -2,7 +2,7 @@
 
 var style_cookie_name = "style" ;
 var style_cookie_duration = 30 ;
-var style_domain = "http://aliciaaarnio.github.io/" ;
+var style_domain = "http://aliciaaarnio.solar/" ;
 
 // *** END OF CUSTOMISABLE SECTION ***
 // You do not need to customise anything below this line
